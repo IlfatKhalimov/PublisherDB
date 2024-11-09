f87a6f116df067da711f36f63bb86dc11099319e
Ilfat Khalimov <ilf-khalimov@list.ru>
2024-11-09 14:42:15 +0300

    Publishers_db.py was upgrated

--- Publishers_db.py
+++ Publishers_db.py
@@ -1,8 +1,7 @@
 import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-# import datetime
-# from datetime import date, datetime
+from datetime import date
 
 Base = declarative_base()
 
@@ -22,25 +21,25 @@ class Book(Base):
     __tablename__ = "book"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.Text, nullable=False)
+    title = sq.Column(sq.String(length=40), nullable=False)
     publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
     # publisher = relationship(Publisher, back_populates="book")
     publisher = relationship(Publisher, backref="book")
 
     def __str__(self):
-        return f'{self.id}: {self.title}'
+        return f'{self.id}: {self.title}: {self.publisher_id}'
 
 class Shop(Base):
     __tablename__ = "shop"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
+    logo = sq.Column(sq.String(length=40), unique=True)
 
     # stock = relationship("Stock", back_populates="shop")
 
     def __str__(self):
-        return f'{self.id}: {self.name}'
+        return f'{self.id}: {self.logo}'
 
 class Stock(Base):
     __tablename__ = "stock"
@@ -63,7 +62,7 @@ class Sale(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Numeric, nullable=False)
-    sale_date = sq.Column(sq.String(length=40), nullable=False)
+    sale_date = sq.Column(sq.Date, nullable=False)
     stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
@@ -77,20 +76,37 @@ def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
+def get_booksales(var):
+    qq = session.query(Stock.shop_id, Shop.logo, Book.title, Publisher.id, Publisher.name, Sale.price, Sale.sale_date
+                       ).join(Shop).join(Book).join(Publisher).join(Sale).subquery()
+    if var.isdigit():
+        qqq = session.query(qq).filter(qq.c.id == var)
+    else:
+        qqq = session.query(qq).filter(qq.c.name == var)
+    column1 = '  Название книги'
+    column2 = '  Магазин'
+    column3 = '  Цена'
+    column4 = '  Дата'
+    print(f"{column1: <40}   {column2: <17}   {column3: <8}   {column4}")
+    for s in qqq.all():
+        # print(s.title, s.logo, s.price, s.sale_date)
+        print(f"{s.title: <40} | {s.logo: <17} | {s.price: <8} | {s.sale_date.strftime('%d-%m-%Y')}")
+
 
 DSN = "postgresql://postgres:bazadannykh@localhost:5432/publishers_db"
 engine = sqlalchemy.create_engine(DSN)
 create_tables(engine)
 
-# сессия
 Session = sessionmaker(bind=engine)
 session = Session()
 
 if __name__ == '__main__':
-    # создание объектов
+    # создание объектов:
     pushkin = Publisher(name="Пушкин")
+    blok = Publisher(name="Блок")
     lermontov = Publisher(name="Лермонтов")
     tolstoy = Publisher(name="Толстой")
+    session.add_all([pushkin, blok, lermontov, tolstoy])
 
     book1 = Book(title="Капитанская дочка", publisher=pushkin)
     book2 = Book(title="Евгений Онегин", publisher=pushkin)
@@ -101,11 +117,15 @@ if __name__ == '__main__':
     book7 = Book(title="Война и мир", publisher=tolstoy)
     book8 = Book(title="Анна Каренина", publisher=tolstoy)
     book9 = Book(title="Филипок", publisher=tolstoy)
+    session.add_all([book1, book2, book3, book4, book5, book6, book7, book8, book9])
 
-    bukvoed = Shop(name="Буквоед")
-    labirint = Shop(name="Лабиринт")
-    knizdom = Shop(name="Книжный дом")
-    chitgor = Shop(name="Читай-город")
+    bukvoed = Shop(logo="Буквоед")
+    labirint = Shop(logo="Лабиринт")
+    knizdom = Shop(logo="Книжный дом")
+    chitgor = Shop(logo="Читай-город")
+    kniga = Shop(logo="Книга")
+    bookstore = Shop(logo="Букстор")
+    session.add_all([bukvoed, labirint, knizdom, chitgor, kniga, bookstore])
 
     stock1 = Stock(book=book1, shop=bukvoed, count=6)
     stock2 = Stock(book=book1, shop=labirint, count=4)
@@ -113,45 +133,25 @@ if __name__ == '__main__':
     stock4 = Stock(book=book7, shop=chitgor, count=20)
     stock5 = Stock(book=book7, shop=knizdom, count=25)
     stock6 = Stock(book=book7, shop=bukvoed, count=26)
-    stock7 = Stock(book=book9, shop=bukvoed, count=27)
+    stock7 = Stock(book=book9, shop=bukvoed, count=11)
     stock8 = Stock(book=book8, shop=labirint, count=5)
     stock9 = Stock(book=book4, shop=chitgor, count=9)
+    stock10 = Stock(book=book7, shop=labirint, count=28)
+    session.add_all([stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9, stock10])
 
-    sale1 = Sale(price=900, sale_date="25-03-2024", stock=stock2, count=4)
-    sale2 = Sale(price=650, sale_date="14-04-2024", stock=stock1, count=6)
-    sale3 = Sale(price=1101, sale_date="16-05-2024", stock=stock4, count=2)
-    sale4 = Sale(price=1102, sale_date="20-05-2024", stock=stock5, count=3)
-    sale5 = Sale(price=1103, sale_date="28-05-2024", stock=stock4, count=5)
-    sale6 = Sale(price=1200, sale_date="01-06-2024", stock=stock7, count=7)
-    sale7 = Sale(price=1200, sale_date="01-06-2024", stock=stock8, count=7)
-    sale8 = Sale(price=1102, sale_date="02-06-2024", stock=stock9, count=1)
+    sale1 = Sale(price=900, sale_date=date(2024, 3, 25), stock=stock2, count=4)
+    sale2 = Sale(price=650, sale_date=date(2024, 4, 14), stock=stock1, count=6)
+    sale3 = Sale(price=1101, sale_date=date(2024, 5,16), stock=stock4, count=2)
+    sale4 = Sale(price=1102, sale_date=date(2024, 5, 20), stock=stock5, count=3)
+    sale5 = Sale(price=1103, sale_date=date(2024, 5, 28), stock=stock3, count=5)
+    sale6 = Sale(price=1200, sale_date=date(2024, 6, 1), stock=stock7, count=7)
+    sale7 = Sale(price=1200, sale_date=date(2024, 6, 1), stock=stock8, count=7)
+    sale8 = Sale(price=1102, sale_date=date(2024, 6, 2), stock=stock9, count=1)
+    session.add_all([sale1, sale2, sale3, sale4, sale5, sale6, sale7, sale8])
 
-    session.add_all([pushkin, lermontov, tolstoy, book1, book2, book3, book4, book5, book6, book7, book8, book9,
-                     bukvoed, labirint, knizdom, chitgor, stock1, stock2, stock3, stock4, sale1, sale2, sale1])
-    session.commit()  # фиксируем изменения
-
-    autor = input("Введите имя автора: ")
-
-    subq0 = session.query(Book).join(Publisher.book).filter(Publisher.name == autor)
-    for s in subq0.all():
-        query_list = []
-        # query_list.append(s.title)
-        # print(query_list)
-        # print(s)
-        subq_stock = session.query(Stock).join(Book.stock).filter(Stock.book_id == s.id)
-        for s_st in subq_stock.all():
-            # query_list.append(s.title)
-            # print(s_st)
-            subq_shop = session.query(Shop).join(Stock.shop).filter(Shop.id == s_st.shop_id)
-            for s_sh in subq_shop.all():
-                # query_list.append(s_sh.name)
-                # print(query_list)
-                subq_sale = session.query(Sale).join(Stock.sale).filter(Sale.stock_id == s_st.id)
-                for s_sa in subq_sale.all():
-                    # print(s_sa)
-                    query_list.append(s.title)
-                    query_list.append(s_sh.name)
-                    query_list.append(s_sa.price)
-                    query_list.append(s_sa.sale_date)
-                    print(' | '.join(map(str, query_list)))
-                    query_list = []
+    # session.add_all([pushkin, lermontov, tolstoy, book1, book2, book3, book4, book5, book6, book7, book8, book9,
+    #                  bukvoed, labirint, knizdom, chitgor, kniga, stock1, stock2, stock3, stock4, stock5, stock6,
+    #                  stock7, stock8, stock9, sale1, sale2, sale3, sale4, sale5, sale6, sale7, sale8])
+    session.commit()
+    autor = input("Введите id или имя автора: ")
+    get_booksales(autor)
